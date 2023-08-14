0fec277828811476a978df627a69ea8d1bae3f03
jun 3 <97373683+jun-ro@users.noreply.github.com>
2023-08-13 20:46:48 -0400

    Delete Server.server.lua

--- Server/Server.server.lua
+++
@@ -1,4 +0,0 @@
-local Polarix = require(game:GetService("ReplicatedStorage").Polarix).new()
-local Proxy = game:GetService("ReplicatedStorage").Polarix.Events.Proxy
-
-Polarix:InitiateServer(Polarix.defaultConfig, Proxy, require(script.EventsList))
